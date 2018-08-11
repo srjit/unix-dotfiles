661b14980a7beaa73c3bfdc245f6e92129518122
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-08-11 15:56:43 -0400

    todo script

---
+++ i3/scripts/todo.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+from taskw import TaskWarrior
+import os
+import subprocess
+
+
+__author__ = "Sreejith Sreekumar"
+__email__ = "sreekumar.s@husky.neu.edu"
+__version__ = "0.0.1"
+
+def sendmessage(message):
+    subprocess.Popen(['notify-send', message])
+    return
+
+try:
+
+    w = TaskWarrior()
+    tasks = w.load_tasks()
+    tasks.keys()
+    print("Tasks:" + str(len(tasks['pending'])))
+
+    if os.environ.get('BLOCK_BUTTON'):
+        pending = tasks['pending']
+        text = ""
+        for task in pending:
+            print(task)
+            text += str(task["id"]) + ". " + task["description"] + "\n"
+
+        sendmessage(text)
+        
+except:
+    pass
+        
+            
