bd4343548bd8a126d82cc36cc8b0f2481c02968d
Sreejith Sreekumar <sreejith2904@gmail.com>
2022-04-27 10:10:36 -0400

    Update emacs shell path to use Mac shell paths

--- emacs-nano/scripts/custom.el
+++ emacs-nano/scripts/custom.el
@@ -1,16 +1,16 @@
 ;; ;; set keybindings
 (load-file "~/.emacs.d/scripts/keybindings.el")
-(load-file "~/.emacs.d/scripts/elegance.el")
+;;(load-file "~/.emacs.d/scripts/elegance.el")
 
 (load-file "~/.emacs.d/scripts/styling.el")
-(load-file "~/.emacs.d/scripts/fira-code-mode.el")
+;; (load-file "~/.emacs.d/scripts/fira-code-mode.el")
 (put 'scroll-left 'disabled nil)
 
 
 (setq user-full-name "Sreejith Sreekumar")
 (setq user-mail-address "sreejith2904@gmail.com")
 
-(add-hook 'python-mode-hook 'fira-code-mode)
+;; (add-hook 'python-mode-hook 'fira-code-mode)
 (xclip-mode 1)
 (undo-tree-mode 1)
 
@@ -18,30 +18,50 @@
 (setenv "SHELL" "/bin/zsh")
 (setq shell-file-name "/bin/zsh")
 
+
 (defun ipython ()
   (interactive)
-  (term "/Users/d107520/venvs/mllib/bin/ipython"))
-(setq python-shell-interpreter "/Users/d107520/venvs/mllib/bin/ipython")
+  (term "/Users/sresreek1/venvs/mllib/bin/ipython"))
+(setq python-shell-interpreter "/Users/sresreek1/venvs/mllib/bin/ipython")
 
 
 
 (require 'virtualenvwrapper)
 (venv-initialize-interactive-shells)
-(setq venv-location "/Users/d107520/venvs/")
-
-;; (load-file "~/.emacs.d/scripts/ox-ipynb.el")
-;; (display-time-mode 1)
-;; (display-battery-mode 1)
+(setq venv-location "/Users/sresreek1/venvs/")
 
 
 (setq company-idle-delay 0)
 (setq recentf-max-menu-items 10)
 
-(load-file "~/.emacs.d/scripts/orgmode.el")
+(global-undo-tree-mode)
 
-(setenv "PATH" "/usr/local/bin:/Library/TeX/texbin/:$PATH" t)
 
-;;(require 'org-bullets)
-;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(global-hl-line-mode 1)
+(set-face-background 'highlight "#222")
+(set-face-foreground 'highlight nil)
+(set-face-underline-p 'highlight t)
+
+;;(setenv "PATH" "/usr/local/bin:/Library/TeX/texbin/:$PATH" t)
+
+
+;; Eshell variables have to the the path in zshrc
+
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$" "" (shell-command-to-string
+					  "$SHELL --login -c 'echo $PATH'"
+						    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(set-exec-path-from-shell-PATH)
+
+
 (provide 'custom)
 ;;;  custom.el ends here
