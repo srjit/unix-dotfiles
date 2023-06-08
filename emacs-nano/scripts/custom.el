37ae3adb04cbb298d7543d7ce1deda5f86e3839b
Sreejith Sreekumar <sreejith2904@gmail.com>
2023-06-08 00:26:40 -0700

    Update custom.el

--- emacs-nano/scripts/custom.el
+++ emacs-nano/scripts/custom.el
@@ -144,10 +144,6 @@
   (load-theme 'solarized-light t)
   (let ((line (face-attribute 'mode-line :underline)))
     (set-face-attribute 'mode-line          nil :overline   line)
-    ;; (set-face-attribute 'mode-line-inactive nil :overline   line)
-    ;; (set-face-attribute 'mode-line-inactive nil :underline  line)
-    ;; (set-face-attribute 'mode-line          nil :box        nil)
-    ;; (set-face-attribute 'mode-line-inactive nil :box        nil)
     (set-face-attribute 'mode-line-inactive nil :background "#f9f2d9")))
 
 (use-package moody
